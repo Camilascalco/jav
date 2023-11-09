8a633b66714b68d7477c76e14de8abafd3bb6f46
Camilascalco <128312038+Camilascalco@users.noreply.github.com>
2023-11-09 08:50:39 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 function soma_seq(){
+    
+    alert(entrou);
+    var n = parseInt(numero.value);
+    var soma = n;
 
-    var n= parseInt(numero.value);
-    var soma=n;
-
-    while (n>1) {
-        soma=soma+(n-1);
-        n=n-1;
-        
+while(n > 1) {
+        soma = soma+(n-1);
+        n = n-1;
     }
+
     alert(soma);
 }
 
-function fat_q(){
-    var n=parseInt(camila.value);
-    var fat=n;
 
+function potencia(){
+    let n=parseInt(numero3.value);
+    let pote = n;
     while(n>1){
-        fat=fat*(n-1);
-        n=n-1;
+        pote = Math.pow(2,n);
+        n = n - 1;
+        alert(pote);
+
     }
-    alert(fat)
 }
